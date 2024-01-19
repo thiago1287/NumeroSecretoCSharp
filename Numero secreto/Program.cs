183ac38a8d6ef2c7f049fc94b77f06e6b824d706
Thiago <thiagosilva1@unipam.edu.br>
2024-01-19 12:15:28 -0300

    COMPLETO

---
+++ Numero secreto/Program.cs
@@ -0,0 +1,23 @@
+﻿Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 101);
+
+do
+{
+    Console.Write("Digite um número entre 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine()!);
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("Parabéns!!!! Você acertou o número.");
+        break;
+    }
+    else if (chute < numeroSecreto)
+    {
+        Console.WriteLine("O número secreto é maior.");
+    }
+    else if (chute > numeroSecreto)
+    {
+        Console.WriteLine("O número secreto é menor.");
+    }
+}
+while (true);
